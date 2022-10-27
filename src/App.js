2829cf5aea684449ea2dcc68ba9380807f43cbc6
DiabloDraco <mirmuhsinmirahmatov@gmail.com>
2022-10-28 00:39:23 +0500

    add

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./container/header";
 import './assets/settings/settings.css'
 import Footer from "./container/footer";
-import './assets/settings/normolize.css'
+import './assets/settings/settings.css'
 
 function App() {
   return (
     <>
-      <Header />
-      <div className="container">
+      <div className="wrapper">
+        <Header />
+        <div className="container main">
 
+        </div>
+        <Footer />
       </div>
-      <Footer />
     </>
   );
 }
--- src/container/footer/footer.js
+++ src/container/footer/footer.js
@@ -1,8 +1,105 @@
 import './footer.css'
 
 function Footer() {
-    return(
+    return (
         <>
+            <div className='footer__wrapper'>
+                <div className='footer__top'>
+                    <div className='footer__top-inner-wrapper container'>
+                        <ul className='footer__top-list'>
+                            <li className='footer__top-item'>
+                                <a className='footer__item-link'>
+                                    CAPITAL INVESTMENT GROUP, INC.
+                                </a>
+                            </li>
+                            <li className='footer__top-item'>
+                                <a className='footer__item-link'>
+                                    CAPITAL INVESTMENT GROUP, INC.
+                                </a>
+                            </li>
+                            <li className='footer__top-item'>
+                                <a className='footer__item-link'>
+                                    CAPITAL INVESTMENT GROUP, INC.
+                                </a>
+                            </li>
+                            <li className='footer__top-item'>
+                                <a className='footer__item-link'>
+                                    CAPITAL INVESTMENT GROUP, INC.
+                                </a>
+                            </li>
+                            <li className='footer__top-item'>
+                                <a className='footer__item-link'>
+                                    CAPITAL INVESTMENT GROUP, INC.
+                                </a>
+                            </li>
+                            <li className='footer__top-item'>
+                                <a className='footer__item-link'>
+                                    CAPITAL INVESTMENT GROUP, INC.
+                                </a>
+                            </li>
+                            <li className='footer__top-item'>
+                                <a className='footer__item-link'>
+                                    CAPITAL INVESTMENT GROUP, INC.
+                                </a>
+                            </li>
+                            <li className='footer__top-item'>
+                                <a className='footer__item-link'>
+                                    CAPITAL INVESTMENT GROUP, INC.
+                                </a>
+                            </li>
+                            <li className='footer__top-item'>
+                                <a className='footer__item-link'>
+                                    CAPITAL INVESTMENT GROUP, INC.
+                                </a>
+                            </li>
+                            <li className='footer__top-item'>
+                                <a className='footer__item-link'>
+                                    CAPITAL INVESTMENT GROUP, INC.
+                                </a>
+                            </li>
+                            <li className='footer__top-item'>
+                                <a className='footer__item-link'>
+                                    CAPITAL INVESTMENT GROUP, INC.
+                                </a>
+                            </li>
+                            <li className='footer__top-item'>
+                                <a className='footer__item-link'>
+                                    CAPITAL INVESTMENT GROUP, INC.
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className='footer__bottom'>
+                    <div className='footer__bottom-inner-wrapper container'>
+                        <p className='footer__bottom-left'>
+                            All rights Reserved. © 2022 Hillhouse-capital
+                        </p>
+                        <ul className='footer__bottom-right'>
+                            <li className='footer__bottom-item'>
+                                <a className='footer__bottom-link'>
+                                    Privacy
+                                </a>
+                            </li>
+                            <li className='footer__bottom-item'>
+                                <a className='footer__bottom-link'>
+                                    Important Disclosures
+                                </a>
+                            </li>
+                            <li className='footer__bottom-item'>
+                                <a className='footer__bottom-link'>
+                                    Contact
+                                </a>
+                            </li>
+                            <li className='footer__bottom-item'>
+                                <a className='footer__bottom-link'>
+                                    The Form CRS
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
