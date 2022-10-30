8b6eee4f1c38f7e3f8dd5d1fae802e3e43af3843
DiabloDraco <mirmuhsinmirahmatov@gmail.com>
2022-10-30 14:42:46 +0500

    added registration page

--- src/pages/registration/registration.js
+++ src/pages/registration/registration.js
@@ -5,10 +5,10 @@ import { useFormik } from 'formik'
 import { useEffect, useRef } from 'react'
 
 function Registration() {
-    useEffect(()=> {
+    useEffect(() => {
         document.querySelector(".header-wrapper").remove()
         document.querySelector(".footer__wrapper").remove()
-    },[])
+    }, [])
     let form = useRef(null)
     let name = useRef(null)
     let last = useRef(null)
@@ -30,6 +30,17 @@ function Registration() {
                 password.current.value,
                 confirm.current.value
             );
+            document.querySelector(".password__error").style.display = 'none'
+        }else{
+            document.querySelector(".password__error").style.display = 'flex'
+        }
+    }
+
+    function error(e) {
+        if (!(e.target.value.length > 0)) {
+            e.target.classList.add("visiblee")
+        }else{
+            e.target.classList.remove("visiblee")
         }
     }
 
@@ -39,32 +50,33 @@ function Registration() {
                 <img className='reg__logo' src={fullLogo} alt="logo" />
                 <form onSubmit={postInfo} ref={form} className="registration__form">
                     <label className='reg__label'>
-                        <input ref={name} className='reg__input' type="text" placeholder='First Name' />
-                        <div className='error__name error'></div>
+                        <input onChange={error} ref={name} className='reg__input' required type="text" placeholder='First Name' />
+                        <div className='error__name error'>Please complete all required fields!</div>
                     </label>
                     <label className='reg__label'>
-                        <input ref={last} className='reg__input' type="text" placeholder='Last Name' />
-                        <div className='error__last error'></div>
+                        <input onChange={error} ref={last} className='reg__input' required type="text" placeholder='Last Name' />
+                        <div className='error__last error'>Please complete all required fields!</div>
                     </label>
                     <label className='reg__label'>
-                        <input ref={mail} className='reg__input' type="email" placeholder='Email' />
-                        <div className='error__mail error'></div>
+                        <input onChange={error} ref={mail} className='reg__input' required type="email" placeholder='Email' />
+                        <div className='error__mail error'>Please complete all required fields!</div>
                     </label>
                     <label className='reg__label'>
-                        <input ref={phone} className='reg__input' type="tel" placeholder='Phone' />
-                        <div className='error__phone error'></div>
+                        <input onChange={error} ref={phone} className='reg__input' required type="tel" placeholder='Phone' />
+                        <div className='error__phone error'>Please complete all required fields!</div>
                     </label>
                     <label className='reg__label'>
-                        <input ref={country} className='reg__input' type="text" placeholder='Country' />
-                        <div className='error__country error'></div>
+                        <input onChange={error} ref={country} className='reg__input' required type="text" placeholder='Country' />
+                        <div className='error__country error'>Please complete all required fields!</div>
                     </label>
                     <label className='reg__label'>
-                        <input ref={password} className='reg__input' type="password" placeholder='Password' />
-                        <div className='error__password error'></div>
+                        <input onChange={error} ref={password} className='reg__input' required type="password" placeholder='Password' />
+                        <div className='error__password error'>Please complete all required fields!</div>
+                        <div className='error__confirm password__error error'>Passwords do not match</div>
                     </label>
                     <label className='reg__label m'>
-                        <input ref={confirm} className='reg__input' type="password" placeholder='Confirm Password' />
-                        <div className='error__confirm error'></div>
+                        <input onChange={error} ref={confirm} className='reg__input' required type="password" placeholder='Confirm Password' />
+                        <div className='error__confirm error'>Please complete all required fields!</div>
                     </label>
                     <button className='submit__btn' type='submit'>Register</button>
                 </form>
