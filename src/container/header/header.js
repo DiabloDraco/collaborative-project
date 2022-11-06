69556a748d439133ec9c7262ca97c26b7a77c64b
DiabloDraco <mirmuhsinmirahmatov@gmail.com>
2022-11-06 20:51:43 +0500

    bug fixed

--- src/container/header/header.js
+++ src/container/header/header.js
@@ -17,14 +17,14 @@ function Header() {
                             <a className='header__left-mail' href='mailto:kaban.jobs@gmail.com'>E-Mail: capital@hillhouse-capital.com</a>
                         </div>
                         <div className='header__right'>
-                            <Link to={'/login'} target="_blank" className='header__right-login'>
+                            <a href='/login' target="_blank" rel="noopener noreferrer" className='header__right-login'>
                                 <img className='header__login-icon' src={Login} alt="Login" />
                                 LOGIN
-                            </Link>
-                            <Link target='_blank' to={'/registraion'} className='header__right-register'>
+                            </a>
+                            <a href='/registraion' rel="noopener noreferrer" target='_blank'  className='header__right-register'>
                                 <img className='header__register-icon' src={Register} alt="Register" />
                                 REGISTER
-                            </Link>
+                            </a>
                         </div>
                     </div>
                 </div>
