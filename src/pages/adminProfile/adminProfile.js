20a9fa8e28970156c55c7bc77a175fe528006d1d
DiabloDraco <mirmuhsinmirahmatov@gmail.com>
2022-11-22 23:27:01 +0500

    added

--- src/pages/adminProfile/adminProfile.js
+++ src/pages/adminProfile/adminProfile.js
@@ -1,8 +1,7 @@
 import styles from './adminProfile.module.css'
-import AdminHeader from '../../components/adminHeader'
-import Sidebar from '../../components/sidebar'
 import { useEffect, useRef, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
+import logo from './../../assets/images/logo.svg'
 
 
 
@@ -92,56 +91,65 @@ function AdminProfile() {
                 (data) ? (
                     <div className={styles.maxwrap}>
                         <div className={styles.fixed}>
-                            <Sidebar />
-                            <div className={`${styles.right__wrapper} ${styles.container__right}`}>
-                                <AdminHeader />
-                                <table className={`${styles.table} ${styles.table_light}`}>
-                                    <thead>
-                                        <tr>
-                                            <th style={{ width: '100px' }} scope='col'>#id</th>
-                                            <th scope='col'>Name</th>
-                                            <th scope='col'>Last Name</th>
-                                            <th scope='col'>Password</th>
-                                            <th scope='col'>Phone</th>
-                                            <th scope='col'>Country</th>
-                                            <th style={{ width: "300px" }} scope='col'>Email</th>
-                                            <th scope='col'>Balance</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {
-                                            data?.map((item) => (
-                                                <tr key={item.user_id}>
-                                                    {console.log(item)}
-                                                    <td style={{ width: "100px" }}>{item?.user_id}</td>
-                                                    <td>{item?.username}</td>
-                                                    <td>{item?.lastname}</td>
-                                                    <td>{item?.password}</td>
-                                                    <td>{item?.contact}</td>
-                                                    <td>{item?.country}</td>
-                                                    <td style={{ width: "300px" }}>{item?.email}</td>
-                                                    <td>{item?.score}</td>
-                                                </tr>
-                                            ))
-                                        }
-                                    </tbody>
-                                </table>
-                                <div>
-                                    <form style={{ display: "flex", flexDirection: "column", gap: "20px", marginBottom: "50px", marginTop: "50px", alignItems: "center" }} onSubmit={changeVal}>
-                                        <input ref={id} style={{ maxWidth: "800px" }} placeholder='userID' type="text" className='log__input' />
-                                        <input ref={userName} style={{ maxWidth: "800px" }} placeholder='Changed Name' type="text" className='log__input' />
-                                        <input ref={lastName} style={{ maxWidth: "800px" }} placeholder='Changed LastName' type="text" className='log__input' />
-                                        <input ref={email} style={{ maxWidth: "800px" }} placeholder='Changed Email' type="text" className='log__input' />
-                                        <input ref={password} style={{ maxWidth: "800px" }} placeholder='Changed Password' type="text" className='log__input' />
-                                        <input ref={balanceChange} style={{ maxWidth: "800px" }} placeholder='Changed Balance' type="text" className='log__input' />
-                                        <button style={{ maxWidth: "330px" }} type='submit' className='profile__button-logout'>Change</button>
-                                    </form>
-                                    <form style={{ display: "flex", flexDirection: "column", gap: "20px", marginBottom: "50px", marginTop: "50px", alignItems: "center" }} onSubmit={userDelete}>
-                                        <input ref={userId} style={{ maxWidth: "800px" }} placeholder='userID' type="text" className='log__input' />
-                                        <button style={{ maxWidth: "330px" }} type='submit' className='profile__button-logout'>Delete</button>
-                                    </form>
-                                </div>
+                            <header className={styles.admin__profile_header}>
+                                <NavLink className={"header__nav-link"} to={'/'}>
+                                    <img style={{marginRight:"70px"}} width={50} height={50} src={logo} alt="" />
+                                </NavLink>
+                                <NavLink className={"header__nav-link"} to={'/'}>
+                                    HOME
+                                </NavLink>
+                                <h2 style={{marginLeft: "550px" , fontSize:"30px" , fontWeight:"700"}} className='compilance__title'>Admin</h2>
+                            </header>
+                            <h2 style={{ marginLeft:"30px" , fontSize:"20px" , fontWeight:"700"}} className='compilance__title'>Admin Table</h2>
+                            <table className={`${styles.table} ${styles.table_light}`}>
+                                <thead>
+                                    <tr>
+                                        <th style={{ width: '50px' }} scope='col'>#id</th>
+                                        <th scope='col'>Name</th>
+                                        <th scope='col'>Last Name</th>
+                                        <th scope='col'>Password</th>
+                                        <th style={{ width: '150px' }} scope='col'>Phone</th>
+                                        <th style={{ width: '80px' }} scope='col'>Country</th>
+                                        <th style={{ width: "250px" }} scope='col'>Email</th>
+                                        <th style={{ width: '80px' }} scope='col'>Balance</th>
+                                        <th scope='col'>Created at</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        data?.map((item) => (
+                                            <tr key={item.user_id}>
+                                                {console.log(item)}
+                                                <td style={{ width: "50px" }}>{item?.user_id}</td>
+                                                <td>{item?.username}</td>
+                                                <td>{item?.lastname}</td>
+                                                <td>{item?.password}</td>
+                                                <td style={{ width: '150px' }}>{item?.contact}</td>
+                                                <td style={{ width: '80px' }}>{item?.country}</td>
+                                                <td style={{ width: "250px" }}>{item?.email}</td>
+                                                <td style={{ width: '80px' }}>{item?.score}</td>
+                                                <td>{item?.created_at}</td>
+                                            </tr>
+                                        ))
+                                    }
+                                </tbody>
+                            </table>
+                            <div>
+                                <form style={{ display: "flex", flexDirection: "column", gap: "20px", marginBottom: "50px", marginTop: "50px", alignItems: "center" }} onSubmit={changeVal}>
+                                    <input ref={id} style={{ maxWidth: "800px" }} placeholder='userID' type="text" className='log__input' />
+                                    <input ref={userName} style={{ maxWidth: "800px" }} placeholder='Changed Name' type="text" className='log__input' />
+                                    <input ref={lastName} style={{ maxWidth: "800px" }} placeholder='Changed LastName' type="text" className='log__input' />
+                                    <input ref={email} style={{ maxWidth: "800px" }} placeholder='Changed Email' type="text" className='log__input' />
+                                    <input ref={password} style={{ maxWidth: "800px" }} placeholder='Changed Password' type="text" className='log__input' />
+                                    <input ref={balanceChange} style={{ maxWidth: "800px" }} placeholder='Changed Balance' type="text" className='log__input' />
+                                    <button style={{ maxWidth: "330px" }} type='submit' className='profile__button-logout'>Change</button>
+                                </form>
+                                <form style={{ display: "flex", flexDirection: "column", gap: "20px", marginBottom: "50px", marginTop: "50px", alignItems: "center" }} onSubmit={userDelete}>
+                                    <input ref={userId} style={{ maxWidth: "800px" }} placeholder='userID' type="text" className='log__input' />
+                                    <button style={{ maxWidth: "330px" }} type='submit' className='profile__button-logout'>Delete</button>
+                                </form>
                             </div>
+
                         </div>
                     </div>
                 ) : (<div style={{ paddingTop: "250px" }} className='container'>
