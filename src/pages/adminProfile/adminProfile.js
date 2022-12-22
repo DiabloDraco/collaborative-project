c3ea3733e23d0e2e93cc4eaf12ff50a5efd9fc05
Mirmuhsin <99722067+DiabloDraco@users.noreply.github.com>
2022-12-22 15:45:23 +0500

    added

--- src/pages/adminProfile/adminProfile.js
+++ src/pages/adminProfile/adminProfile.js
@@ -22,7 +22,7 @@ function AdminProfile() {
             }
         })
             .then(req => req.json())
-            .then(data => setData(data.data.sort((a ,b)=>{
+            .then(data => setData(data.data.sort((a, b) => {
                 return a.user_id - b.user_id
             })))
     }
@@ -49,8 +49,8 @@ function AdminProfile() {
                 "tempId": e.target.parentNode.dataset.id
             })
         })
-        .then(req => req.json())
-        .then(data => window.location.reload())
+            .then(req => req.json())
+            .then(data => window.location.reload())
     }
 
     function reject(e) {
@@ -61,8 +61,8 @@ function AdminProfile() {
                 "token": `${JSON.parse(localStorage.getItem('admin'))}`
             }
         })
-        .then(req => req.json())
-        .then(data => window.location.reload())
+            .then(req => req.json())
+            .then(data => window.location.reload())
     }
 
     let id = useRef(null)
@@ -72,7 +72,49 @@ function AdminProfile() {
     let email = useRef(null)
     let password = useRef(null)
     let balanceChange = useRef(null)
+    let phoneId = useRef(null)
+    let findedWrapper = useRef(null)
 
+    function userSearch(e) {
+        e.preventDefault()
+
+        findedWrapper.current.innerHTML = null
+        let num = phoneId.current.value
+        let filtered = data?.filter(user => user.contact == num)
+
+        for (const item of filtered) {
+
+            let li = document.createElement("li")
+            li.classList.add(`search__item`)
+            let p = document.createElement("p")
+            let p1 = document.createElement("p")
+            let p2 = document.createElement("p")
+            let p3 = document.createElement("p")
+            let p4 = document.createElement("p")
+            let p5 = document.createElement("p")
+            let p6 = document.createElement("p")
+            let p7 = document.createElement("p")
+            let p8 = document.createElement("p")
+            p.textContent = `UserId: ${item?.user_id}`
+            p1.textContent = `User name: ${item?.username}`
+            p2.textContent = `User Last name: ${item?.lastname}`
+            p3.textContent = `User password: ${item?.password}`
+            p4.textContent = `User Contact: ${item?.contact}`
+            p5.textContent = `User email: ${item?.email}`
+            p6.textContent = `User score: ${item?.score}`
+            p7.textContent = `User Country: ${item?.country}`
+            p8.textContent = `User created at: ${item?.created_at}`
+            li.appendChild(p1)
+            li.appendChild(p2)
+            li.appendChild(p3)
+            li.appendChild(p4)
+            li.appendChild(p5)
+            li.appendChild(p6)
+            li.appendChild(p7)
+            li.appendChild(p8)
+            findedWrapper.current.appendChild(li)
+        }
+    }
 
     useEffect(() => {
         if (!JSON.parse(localStorage.getItem("admin"))) {
@@ -179,6 +221,7 @@ function AdminProfile() {
                             </table>
                             <div>
                                 <form style={{ display: "flex", flexDirection: "column", gap: "20px", marginBottom: "50px", marginTop: "50px", alignItems: "center" }} onSubmit={changeVal}>
+                                    <h3 style={{ textAlign: "center", fontSize: "20px", fontWeight: "700" }} className='compilance__title'>Users Control</h3>
                                     <input ref={id} style={{ maxWidth: "800px" }} placeholder='userID' type="text" className='log__input' />
                                     <input ref={userName} style={{ maxWidth: "800px" }} placeholder='Changed Name' type="text" className='log__input' />
                                     <input ref={lastName} style={{ maxWidth: "800px" }} placeholder='Changed LastName' type="text" className='log__input' />
@@ -187,6 +230,18 @@ function AdminProfile() {
                                     <input ref={balanceChange} style={{ maxWidth: "800px" }} placeholder='Changed Balance' type="text" className='log__input' />
                                     <button style={{ maxWidth: "330px" }} type='submit' className='profile__button-logout'>Change</button>
                                 </form>
+
+                                <h3 style={{ textAlign: "center", fontSize: "20px", fontWeight: "700" }} className='compilance__title'>Search users</h3>
+
+                                <form style={{ display: "flex", flexDirection: "column", gap: "20px", marginBottom: "50px", marginTop: "50px", alignItems: "center" }} onSubmit={userSearch}>
+                                    <input ref={phoneId} style={{ maxWidth: "800px" }} placeholder='Phone' type="text" className='log__input' />
+                                    <button style={{ maxWidth: "330px" }} type='submit' className='profile__button-logout'>Search</button>
+                                </form>
+
+                                <ul style={{ listStyleType: "none", padding: "0", marginLeft: "auto", "marginRight": "auto", width: "430px", height: "350px", overflowY: "scroll" }} ref={findedWrapper}>
+
+                                </ul>
+
                                 <form style={{ display: "flex", flexDirection: "column", gap: "20px", marginBottom: "50px", marginTop: "50px", alignItems: "center" }} onSubmit={userDelete}>
                                     <input ref={userId} style={{ maxWidth: "800px" }} placeholder='userID' type="text" className='log__input' />
                                     <button style={{ maxWidth: "330px" }} type='submit' className='profile__button-logout'>Delete</button>
